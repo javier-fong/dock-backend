7abffe937d77f88966ec9c3a8ac197e989e7185a
Javier Fong <fong.kayyuen@gmail.com>
2020-09-11 00:29:44 +0800

    edit mongo uri

--- db/index.js
+++ db/index.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
-const MONGODB_URI = 'mongodb://localhost:27017/' + 'dock';
-// process.env.MONGODB_URI || 
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/' + 'dock';
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 
