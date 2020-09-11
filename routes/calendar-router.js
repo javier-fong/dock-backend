658e415f05fd66e64d31e5d6fee2a96a9dcf96b6
Javier Fong <fong.kayyuen@gmail.com>
2020-09-12 00:57:08 +0800

    add route

--- routes/calendar-router.js
+++ routes/calendar-router.js
@@ -5,6 +5,7 @@ const CalendarController = require('../controllers/calendar-controller');
 
 router.post('/event', CalendarController.createEvent);
 router.get('/events/:email', CalendarController.getEvents);
+router.get('/oneevent/:email', CalendarController.getOneEvent);
 router.delete('/event/delete/:start/:end/:title', CalendarController.deleteEvent);
 
 module.exports = router;
