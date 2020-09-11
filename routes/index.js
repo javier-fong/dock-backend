5083c89362485fac5bc6e4f436cd59d744327872
Javier Fong <fong.kayyuen@gmail.com>
2020-09-11 19:59:35 +0800

    add calendar router

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ const TodoRouter = require('./todo-router');
 const AuthRouter = require('./auth-router');
 const PhotoJournalRouter = require('./photoJournal-router');
 const UserRouter = require('./user-router');
+const CalendarRouter = require('./calendar-router');
 
 module.exports = {
     TodoRouter,
     AuthRouter,
     PhotoJournalRouter,
-    UserRouter
+    UserRouter,
+    CalendarRouter
 }
