3c0a5981a8c637a6c2fdb170be4f13c0a3ebb80c
Javier Fong <fong.kayyuen@gmail.com>
2020-09-16 14:15:03 +0800

    edit route

--- routes/photoJournal-router.js
+++ routes/photoJournal-router.js
@@ -5,7 +5,7 @@ const PhotoJournalController = require('../controllers/photoJournal-controller')
 
 router.post('/journalpost/create', PhotoJournalController.createJournalPost);
 router.get('/journalposts/:email', PhotoJournalController.getJournalPosts);
-router.get('/onejournalpost/:email', PhotoJournalController.getThreeJournalPost);
+router.get('/threejournalpost/:email', PhotoJournalController.getThreeJournalPost);
 router.put('/journalpost/edit/:id', PhotoJournalController.editJournalPost);
 router.delete('/journalpost/delete/:id', PhotoJournalController.deleteJournalPost);
 
